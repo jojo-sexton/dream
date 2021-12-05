930f0c72c61481e330d02a5699ec5cd37553bb74
iamjojo <jojolsexton@gmail.com>
2021-12-06 08:44:49 +1300

    setup to deploy to heroku

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const server = require('./server')
+const express = require('express')
 
 const port = process.env.PORT || 3000
+
+if (process.env.NODE_ENV === 'production') {
+  server.use(express.static('build'))
+  server.get('*')
+}
 
 server.listen(port, function () {
   // eslint-disable-next-line no-console
